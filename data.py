e77c4f2b1ab36b120bfd3fe6bacdca8bfffbb4f3
ZeoYiDeo <121034549+ZeoYiDeo@users.noreply.github.com>
2025-03-22 20:42:38 +0800

    Update data.py

--- data.py
+++ data.py
@@ -19,8 +19,8 @@ class UserItemRatingDataset(Dataset):
         self.user_tensor = user_tensor
         self.item_tensor = item_tensor
         self.target_tensor = target_tensor
-        self.cv= torch.load('../data/Bili_Food/Bili_Food_vit.pt')
-        self.ct= torch.load('../data/Bili_Food/Bili_Food_bert.pt')
+        self.cv= torch.load('../data/DY/DY_vit.pt')
+        self.ct= torch.load('../data/DY/DY_bert.pt')
     def __getitem__(self, index):
         return self.user_tensor[index], self.item_tensor[index], self.target_tensor[index] , self.cv[int(self.item_tensor[index])], self.ct[int(self.item_tensor[index])]
 
